4346f6b572d770b161ab5e9585078928b63a07d5
Marko Opacic <marko.opacic+github@flabbergast.agency>
2025-10-08 16:59:26 +0200

    clean up Package.swift file

--- Package.swift
+++ Package.swift
@@ -6,42 +6,35 @@ import CompilerPluginSupport
 
 let package = Package(
     name: "EnumReducerView",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
+        .macCatalyst(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "EnumReducerView",
             targets: ["EnumReducerView"]
-        ),
-        .executable(
-            name: "EnumReducerViewClient",
-            targets: ["EnumReducerViewClient"]
-        ),
+        )
     ],
     dependencies: [
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "601.0.0-latest"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", .upToNextMajor(from: "1.17.1"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        // Macro implementation that performs the source transformation of a macro.
         .macro(
             name: "EnumReducerViewMacros",
             dependencies: [
-                .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
             ]
         ),
-
-        // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "EnumReducerView", dependencies: ["EnumReducerViewMacros"]),
-
-        // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "EnumReducerViewClient", dependencies: ["EnumReducerView"]),
-
-        // A test target used to develop the macro implementation.
+        .target(
+            name: "EnumReducerView",
+            dependencies: ["EnumReducerViewMacros"]
+        ),
         .testTarget(
             name: "EnumReducerViewTests",
             dependencies: [
--- Sources/EnumReducerViewClient/main.swift
+++
@@ -1,3 +0,0 @@
-import EnumReducerView
-
-print("EnumReducerView main")
